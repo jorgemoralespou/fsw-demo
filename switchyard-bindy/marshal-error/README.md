b63510b9dac2ac845f718adeed160918f2f87f59
jorgemoralespou <jorgemoralespou@gmail.com>
2014-05-14 14:26:00 +0200

    Reproducer for bindy marshal error

---
+++ switchyard-bindy/marshal-error/README.md
@@ -0,0 +1,43 @@
+# SwitchYard Bindy integration - Marshal error
+This project tests marshaling and unmarshaling of a CSV file to Bean and from Bean to CSV file.
+
+It demonstrates an error when unmarshaling.
+
+## Prerequitites
+
+Install bindy module (for that only dependency is on **camel-bindy-2.10.0.redhat-60024.jar**
+
+## How to test
+
+Create a file in /tmp/csv2 with following contents:
+````
+1|Student 1|1st|80|56|54|90|90
+2|Student 2|1st|70|65|75|90|11
+3|Student 3|1st|60|56|54|90|93
+4|Student 4|1st|50|65|75|90|53
+5|Student 5|1st|40|56|54|90|77
+6|Student 6|1st|30|65|75|90|34
+7|Student 7|1st|40|56|54|90|74
+8|Student 8|1st|50|65|75|90|68
+9|Student 9|1st|60|56|54|90|44
+````
+
+Output: You should get a file in /tmp named marshal2 with following content:
+````
+1|Student 1|1st|A|370|Pass
+2|Student 2|1st|B+|311|Pass
+3|Student 3|1st|A|353|Pass
+4|Student 4|1st|B+|333|Pass
+5|Student 5|1st|B+|317|Pass
+6|Student 6|1st|B|294|Pass
+7|Student 7|1st|B+|314|Pass
+8|Student 8|1st|B+|348|Pass
+9|Student 9|1st|B+|304|Pass
+````
+
+But instead, you will get:
+````
+java.lang.IllegalArgumentException: Some mandatory fields are missing, line: 1: java.lang.IllegalArgumentException: Some mandatory fields are missing, line: 1
+````
+
+It seems that classes for BindyCSVDataFormat must be in separate classes, as it seems there is a bug.
---
+++ switchyard-bindy/marshal-error/src/main/java/com/example/switchyard/bindy/StudentResult.java
@@ -0,0 +1,109 @@
+package com.example.switchyard.bindy;
+
+import java.io.Serializable;
+
+import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
+import org.apache.camel.dataformat.bindy.annotation.DataField;
+
+@CsvRecord(separator = "\\|")
+public class StudentResult implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@DataField(pos = 1, required = true)
+	private String id;
+	
+	@DataField(pos = 2, required = true)
+	private String name;
+	
+	@DataField(pos = 3, required = true)
+	private String semester;
+	
+	@DataField(pos = 4, required = true)
+	private String grade;
+	
+	@DataField(pos = 5, required = true)
+	private long totalScore ;
+	
+	@DataField(pos = 6, required = true)
+	private String result ;
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	public long getTotalScore() {
+		return totalScore;
+	}
+	
+	public void setTotalScore(long totalScore) {
+		this.totalScore = totalScore;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	@Override
+	public String toString() {
+		return "StudentResult [id=" + id + ", name=" + name + ", semester="
+				+ semester + ", grade=" + grade + ", totalScore=" + totalScore
+				+ ", result=" + result + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		StudentResult other = (StudentResult) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
